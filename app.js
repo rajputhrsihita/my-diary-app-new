25515a14ae45548e8fe37ed960a79136cafc222f
hraj0016 <hraj0016@student.monash.edu>
2025-11-19 22:30:45 +1100

    functions for loading and saving data

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ let currentTitle = ''; //temporarily stores the title you are typing. Updates as
 let currentContent = ''; //temporarily stores the content you are typing. Updates as you type in the text area    
 // After clicking "New Entry" our currentView will go in 'edit' mode, we type in our title and content. After we save it our entru will have an id, title, content, and date. After saving our currentView will be back to list and title and content will be empty
 
+//---------------------------------------
+// Load & Save Data 
+//---------------------------------------
+function loadEntries(){
+    //Get the saved data from browser storage. Asks browser if there is any saved diary entries. 
+    const saved = localStorage.getItem('diaryEntries');
+    //If there is saved entries, saved will have a list of id, title, content, and date for every entry.
+
+    //If saved 
+    if (saved) { 
+        //Converts text to JavaScript array 
+        entries = JSON.parse(saved);
+    }
+
+}
+
+function saveEntries() { 
+    //Convert JavaScript to text 
+    //Then save it to browser storage 
+    localStorage.setItem('diaryEntries', JSON.stringify(entries));
+}
